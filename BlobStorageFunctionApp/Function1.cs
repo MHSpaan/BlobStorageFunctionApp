3eec855cd11e0baddfc1001ba97796d90c9fd38a
Matthijs <matthijs.spaan@centric.eu>
2022-04-06 12:42:54 +0200

    Add project files.

---
+++ BlobStorageFunctionApp/Function1.cs
@@ -0,0 +1,92 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace BlobStorageFunctionApp
+{
+    public static class Function1
+    {
+        //static string blobstorageUrl = "https://storagematthijs.blob.core.windows.net/blob1";
+        //public const CloudBlobClient blobClient = getClient();
+        //public Function1()
+        //{
+
+        //}
+        [FunctionName("PostPicture")]
+        public static async Task<IActionResult> PostPicture(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("GetPicture")]
+        public static async Task<IActionResult> GetPicture(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("GetPictureList")]
+        public static async Task<IActionResult> GetPictureList(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        //private static CloudBlobClient getClient()
+        //{
+        //    string storageConnection = ConfigurationManager.ConnectionStrings["AzureStorageConnection"].ToString();
+        //    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnection);
+
+        //    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+        //    return blobClient;
+        //}
+    }
+}
